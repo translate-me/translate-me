6bee1fa9f4d630b86bbaf8b55c248c1544f8f81a
Rômulo Souza <romulovinicius10@gmail.com>
2019-05-09 19:32:53 -0300

    Create method to insert text

--- api/text/views.py
+++ api/text/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from text.models import Text
 from text.serializers import TextSerializer
 
@@ -11,22 +10,20 @@ class TextView(APIView):
     def get(self, request):
         return Response('ok')
 
-    # def post(self, request):
-    #     text_content = self.request.data['text_content']
-    #     breakpoints = self.request.data['breakpoints']
-    #     text_lenght = len(text_content)
-    #
-    #     for i in range(len(breakpoints)-1):
-    #         fragment_content = text_content[breakpoints[i]:breakpoints[i+1]]
-    #         self.create_fragment(fragment_content)
-    #
-    #     last_fragment_content = text_content[breakpoints[-1]:text_lenght]
-    #     self.create_fragment(last_fragment_content)
-    #
-    #     return Response("Fragmentos criados", status=status.HTTP_201_CREATED)
-    #
-    # def create_fragment(self, fragment_content):
-    #     fragment = TextFrag.objects.create(
-    #         content = fragment_content,
-    #         value = len(fragment_content)*0.1,
-)
+    def post(self, request):
+        data = request.data
+        text = data['text']
+        serializer = TextSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Texto inserido com sucesso', status=status.HTTP_201_CREATED)
+        else:
+            return Response('Dados inválidos', status=status.HTTP_400_BAD_REQUEST)
+
+
+# {
+#     "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut tristique ligula. Donec mollis lorem elementum purus semper convallis. Etiam ac lectus ac dolor tempus pellentesque non eu enim. Integer venenatis neque eget massa blandit, iaculis tincidunt tellus egestas. Nunc finibus, elit at sagittis sodales, velit ligula malesuada nulla, eget mollis turpis ipsum sed mi. Cras non condimentum lectus. Suspendisse potenti. Mauris nulla sapien, tempor eget ipsum et, vehicula imperdiet massa. Praesent sed vehicula nulla. Proin sagittis consequat lacus sit amet suscipit. Etiam a risus vel leo venenatis ultricies at semper ante. Maecenas sed odio mauris.",
+#     "context": "Hello World",
+#     "author": 1,
+#     "language": 1
+# }
