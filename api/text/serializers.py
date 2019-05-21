2682b799aaa9ca200036e681da89f03777d9d445
joao robson <joaorobsonmartins@gmail.com>
2019-05-20 21:16:26 -0300

    Add serializer for Fragment
    Co-authored-by: Helena Goulart <helenabretasg@gmail.com>

--- api/text/serializers.py
+++ api/text/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
-from text.models import Text
+from text.models import Text, Fragment
 
 class TextSerializer(serializers.ModelSerializer):
     class Meta:
         model = Text
-        fields = [
-            'context',
-            'author',
-            'language'
-        ]
+        fields = '__all__'
+class FragmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fragment
+        fields = ['id_translator']
+
+
