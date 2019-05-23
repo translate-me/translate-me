b090700de2f719d9e2bc89d6e1f33b002d89b2cf
victorhdcoelho <victorhdcoelho@gmail.com>
2019-05-23 01:00:24 -0300

    Add validation on insert text's method
    Signed-off-by: victorhdcoelho <victorhdcoelho@gmail.com>
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>

--- api/text/models/text_model.py
+++ api/text/models/text_model.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-# Create your models here.
 
 class Text (models.Model):
     context = models.CharField(max_length=200)
     total_fragments = models.IntegerField(default=0)
     finished_fragments = models.IntegerField(default=0)
-    author = models.IntegerField() #id_autor
-    language = models.IntegerField() #id_language
-
+    author = models.CharField(max_length=50)
+    language = models.IntegerField()
--- api/text/views.py
+++ api/text/views.py
@@ -1,34 +1,53 @@
+import socket
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from text.models.fragment_model import Fragment
 from text.models.text_model import Text
 from text.serializers.text_serializer import TextSerializer
+from django.http import JsonResponse
+import requests as rq
 
-# Create your views here.
 
 class TextView(APIView):
     def get(self, request):
         return Response('ok')
 
+    # TODO write swagger function
     def post(self, request):
         data = request.data
+        token = data['token']
+        post_token = {
+            'token': token,
+        }
+        # Validate token
+        host = socket.gethostbyname('authentication_django')
+        resq = rq.post("http://"+host+":8090/user/login/api/v0/validate/",
+                       data=post_token)
+
+        if resq.status_code > 300 or resq.status_code < 200:
+            message = "Server or token not available !"
+            return JsonResponse({'status': False, 'message': message})
+
         text_content = data['text_content']
         breakpoints = data['breakpoints']
         serializer = TextSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            self.fragment_text(text_content, breakpoints, serializer.data['id'])
-            return Response('Texto inserido com sucesso', status=status.HTTP_201_CREATED)
+            self.fragment_text(text_content, breakpoints,
+                               serializer.data['id'])
+            return Response('Texto inserido com sucesso',
+                            status=status.HTTP_201_CREATED)
         else:
-            return Response('Dados inválidos', status=status.HTTP_400_BAD_REQUEST)
+            return Response('Dados inválidos',
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def fragment_text(self, text_content, breakpoints, id_text):
         '''
         Receives a text and splits it into fragments, according to breakpoints
         '''
 
-        text = Text.objects.get(id = id_text)
+        text = Text.objects.get(id=id_text)
         break_point = 0
         for i in breakpoints:
             fragment_content = text_content[break_point:i]
@@ -38,22 +57,32 @@ class TextView(APIView):
         self.create_fragment(last_fragment_content, text)
         text.total_fragments = len(breakpoints) + 1
         text.save()
-        
+
     def create_fragment(self, fragment_content, text):
         '''
         Receives a fragment and saves it in database
         '''
-        fragment = Fragment.objects.create(
-            content = fragment_content,
-            value = len(fragment_content)*0.1,
-            id_text = text
-        )
+        Fragment.objects.create(content=fragment_content,
+                                value=len(fragment_content)*0.1,
+                                id_text=text
+                                )
 
 
-# {
-#     "text_content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut tristique ligula. Donec mollis lorem elementum purus semper convallis. Etiam ac lectus ac dolor tempus pellentesque non eu enim. Integer venenatis neque eget massa blandit, iaculis tincidunt tellus egestas. Nunc finibus, elit at sagittis sodales, velit ligula malesuada nulla, eget mollis turpis ipsum sed mi. Cras non condimentum lectus. Suspendisse potenti. Mauris nulla sapien, tempor eget ipsum et, vehicula imperdiet massa. Praesent sed vehicula nulla. Proin sagittis consequat lacus sit amet suscipit. Etiam a risus vel leo venenatis ultricies at semper ante. Maecenas sed odio mauris.",
-#     "context": "Hello World",
-#     "author": 1,
-#     "language": 1,
-#     "breakpoints": [50, 100, 150, 250, 400, 500]
-# }
+"""
+{
+    "text_content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Aliquam ut tristique ligula. Donec mollis lorem elementum
+    purus semper convallis. Etiam ac lectus ac dolor tempus pellentesque non
+    eu enim. Integer venenatis neque eget massa blandit, iaculis tincidunt
+    tellus egestas. Nunc finibus, elit at sagittis sodales, velit ligula male
+    suada nulla, eget mollis turpis ipsum sed mi. Cras non condimentum lectus
+    . Suspendisse potenti. Mauris nulla sapien, tempor eget ipsum et, vehicul
+    a imperdiet massa. Praesent sed vehicula nulla. Proin sagittis consequat
+    lacus sit amet suscipit. Etiam a risus vel leo venenatis ultricies at
+    semper ante. Maecenas sed odio mauris.",
+    "context": "Hello World",
+    "author": 1,
+    "language": 1,
+    "breakpoints": [50, 100, 150, 250, 400, 500]
+}
+"""
