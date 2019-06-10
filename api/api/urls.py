2d01c206bd239fb63887b7eb81cbaef788e0bb9f
Alexandre Miguel <aleronupe@gmail.com>
2019-06-09 22:23:29 -0300

    Remove unused imports on api/urls

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,20 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Translate.me API',
-        default_version='v0',
-        description='Mudar depois',
-
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
